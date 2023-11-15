cdc5fd874a89826ff914e12fa37026bf35f69751
Neyyss <neyysss@hotmail.com>
2023-11-15 19:59:37 -0300

    change server

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -90,7 +90,7 @@ const handleLogin = (event) => {
     login.style.display  = "none";
     chat.style.display = "flex";
 
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("wss://backend-chat-zi4o.onrender.com");
     websocket.onmessage = processMessage;    
 
 }
